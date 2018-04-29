cdd578a551b1e2f2fa4976121f998625f25349e1
Michal Borciuch <michal123borciuch@gmail.com>
2018-04-29 13:37:50 +0200

    Spring Query Language

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +15,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    UnitOfMeasureRepository unitOfMeasureRepository;
+    CategoryRepository categoryRepository;
+
+    public IndexController(UnitOfMeasureRepository unitOfMeasureRepository, CategoryRepository categoryRepository) {
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Tea-spoon");
+
+        System.out.println("Category Id is: " + categoryOptional.get().getId());
+        System.out.println("Unit of Measure Id is: " + unitOfMeasure.get().getId());
         return "index";
+
     }
+
+
+
 }
--- src/main/java/guru/springframework/repositories/UnitOfMeasuer.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.repositories;
-
-import guru.springframework.domain.UnitOfMeasure;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UnitOfMeasuer extends CrudRepository<UnitOfMeasure, Long> {
-}
